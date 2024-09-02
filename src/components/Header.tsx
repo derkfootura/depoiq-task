aca98cc85cbf8abe7639357e77e67ad52f14f62e
Derk <eilanderk7@gmail.com>
2024-09-02 15:46:44 +0300

    update header for responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,26 +5,22 @@ import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 
 const Header = () => {
   return (
-    <div className='header'>
-      <Tabs
-        items={[
-          { label: 'Cases', key: 'case' },
-          { label: 'Deponents', key: 'deponent' },
-        ]}
-        tabBarExtraContent={{
-          left: (
-            <div className='flex flex-row items-center'>
-              <OrganizationSwitcher />
-              <Logo />
-            </div>
-          ),
-          right: (
-            <div className='flex flex-row items-center gap-3'>
-              <UserButton  />
-            </div>
-          )
-        }}
-      />
+    <div className='header flex flex-col sm:flex-row items-start'>
+      <div className='flex flex-row items-center'>
+        <OrganizationSwitcher />
+        <Logo />
+      </div>
+      <div className='flex flex-row justify-between items-center flex-1 w-full sm:w-fit'>
+        <Tabs
+          items={[
+            { label: 'Cases', key: 'case' },
+            { label: 'Deponents', key: 'deponent' },
+          ]}
+        />
+        <div className='flex flex-row items-center gap-3'>
+          <UserButton />
+        </div>
+      </div>
     </div>
   );
 }
